0a0a64c58d9af8f31f9f1a8d3ed89cf9e9d4da9b
sarahjdevore <sarahjdevore@gmail.com>
2024-10-26 11:00:28 -0600

    Initial tech mom upload
    graduation

---
+++ index.js
@@ -0,0 +1,7 @@
+const greeting = document.getElementById("greeting");
+greeting.style.textAlign = "center";
+greeting.style.marginTop = "50px";
+greeting.style.fontSize = "50px";
+greeting.s
+const username = window.prompt("What's your name?", "Jane");
+greeting.innerHTML = "Welcome to my website, " + username + "!"
